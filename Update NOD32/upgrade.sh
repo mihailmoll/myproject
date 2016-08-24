56e854821c617cfaa60e1028b3ec9c5b67bc6673
mihailmoll <popcornrus281@gmail.com>
2016-08-24 21:50:45 +0300

    Update NOD32

--- Update NOD32/upgrade.sh
+++ Update NOD32/upgrade.sh
@@ -3,6 +3,7 @@ sed "s/<br \/>/ /g" 1.txt > 6.txt
 grep "Username: " 6.txt > 2.txt
 grep "Password: " 6.txt > 3.txt
 grep "Expiration: " 6.txt > 4.txt
-paste 2.txt	3.txt 4.txt > res.txt
-sed "s/'\t'/ /g" res.txt > sell.txt
-mv sell.txt ~/Desktop
+paste 2.txt	3.txt 4.txt > sell.txt
+sed "s/'\t'/ /g" sell.txt > res.txt
+cat res.txt
+rm -f *.txt 
